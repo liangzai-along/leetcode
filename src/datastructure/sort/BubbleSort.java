2a3964e044992677d6a591902877070718ea3c1f
xiaolong <xlshi1996@163.com>
2020-08-03 16:10:48 +0800

    修改了插入排序

--- src/datastructure/sort/BubbleSort.java
+++ src/datastructure/sort/BubbleSort.java
@@ -14,10 +14,6 @@ public class BubbleSort {
     public static void main(String[] args) {
         Integer[] random = Integers.random(10, 1, 100);
 
-        Times.test("冒泡排序",() -> {
-            selectSort(random);
-        });
-
         Integers.println(random);
     }
 
@@ -68,21 +64,4 @@ public class BubbleSort {
         }
     }
 
-    public static void selectSort(Integer[] array){
-
-        for (int end = array.length - 1; end > 0; end--){
-            //记录最大元素位置
-            int maxIndex = 0;
-            for (int begin = 0; begin <= end; begin++){
-                if (array[begin] > array[maxIndex]){
-                    maxIndex = begin;
-                }
-            }
-            //交换位置
-            int temp = array[end];
-            array[end] = array[maxIndex];
-            array[maxIndex] = temp;
-
-        }
-    }
 }
--- src/datastructure/sort/InsertSort.java
+++ src/datastructure/sort/InsertSort.java
@@ -11,16 +11,13 @@ public class InsertSort {
 
     public static void insertSort(int[] array){
 
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < array[i-1]){
-                int temp = array[i];
-                int j = 0;
-                for (j = i -1; i >= 0 && temp < array[j];i --){
-                    //移动
-                    array[j + 1] = array[j];
-                }
-                //插入
-                array[j + 1] = temp;
+        for (int begin = 1; begin < array.length; begin++) {
+            int cur = begin;
+            while (cur > 0 && array[cur-1] > array[cur]){
+                int temp = array[cur];
+                array[cur] = array[cur-1];
+                array[cur-1] = temp;
+                cur--;
             }
         }
     }
