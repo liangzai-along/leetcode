414ccc887c44d55082d09ff91df9ef3e3a7e16fc
xiaolong <xlshi1996@163.com>
2020-08-10 11:02:59 +0800

    牛客网_28_数组中出现次数超过一半的数字-基于Hashmap

--- src/question/array/_28_数组中出现次数超过一半的数字.java
+++ src/question/array/_28_数组中出现次数超过一半的数字.java
@@ -1,5 +1,8 @@
 package question.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author: xiaolong
  * @email: xlshi1996@163.com
@@ -18,17 +21,48 @@ public class _28_数组中出现次数超过一半的数字 {
         if (array == null || array.length == 0) {
             return 0;
         }
+        if (array.length == 1) {
+            return array[0];
+        }
         int[] hashtable = new int[100];
         for (int i = 0; i < array.length; i++) {
             if (hashtable[array[i]] > 0) {
                 hashtable[array[i]] += 1;
+                // 提前结束
+                if (hashtable[array[i]] > array.length/2) {
+                    return array[i];
+                }
             }else {
                 hashtable[array[i]] = 1;
             }
         }
-        for (int i = 0; i < hashtable.length; i++) {
-            if (hashtable[i] > array.length/2){
-                return i;
+        return 0;
+    }
+
+    /**
+     * 基于hashmap
+     * @param array
+     * @return
+     */
+    public int solution(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        if (array.length == 1) {
+            return array[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int value;
+        for (int i = 0; i < array.length; i++) {
+            if (!map.containsKey(array[i])) {
+                map.put(array[i],1);
+            }else {
+                value = map.get(array[i]);
+                map.put(array[i],value +1);
+                // 提前结束
+                if (value + 1 > array.length/2) {
+                    return array[i];
+                }
             }
         }
         return 0;
