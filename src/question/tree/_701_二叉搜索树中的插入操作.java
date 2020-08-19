87876fb66ba71bc19d2819df55b7b59247065a08
xiaolong <xlshi1996@163.com>
2020-08-19 15:41:48 +0800

    leetcode_701_二叉搜索树中的插入操作

---
+++ src/question/tree/_701_二叉搜索树中的插入操作.java
@@ -0,0 +1,23 @@
+package question.tree;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/8/19 10:45
+ * 地址：
+ */
+public class _701_二叉搜索树中的插入操作 {
+    /*
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // 递归只关心这个点做的事
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }*/
+}
