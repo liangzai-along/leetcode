20fe84a5b69f3b7d0f9ae890679a75d785c0c8ed
xiaolong <xlshi1996@163.com>
2020-08-19 16:03:52 +0800

    leetcode_450_删除二叉搜索树中的节点

---
+++ src/question/tree/_23_二叉搜索树的后序遍历序列.java
@@ -0,0 +1,14 @@
+package question.tree;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/8/14 19:32
+ * 地址：https://www.nowcoder.com/practice/a861533d45854474ac791d90e447bafd?tpId=13&&tqId=11176&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class _23_二叉搜索树的后序遍历序列 {
+
+    public boolean VerifySquenceOfBST(int [] sequence) {
+        return  true;
+    }
+}
