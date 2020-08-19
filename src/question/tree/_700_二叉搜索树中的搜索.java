436738d8c04660ae56bbee8eae36d580982d9f3c
xiaolong <xlshi1996@163.com>
2020-08-19 15:33:14 +0800

    leetcode_700_二叉搜索树中的搜索

---
+++ src/question/tree/_700_二叉搜索树中的搜索.java
@@ -0,0 +1,23 @@
+package question.tree;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/8/19 10:45
+ */
+public class _700_二叉搜索树中的搜索 {
+    /**
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) {
+            return root;
+        }else if (root.val > val) {
+            return searchBST(root.left, val);
+        }else {
+            return searchBST(root.right, val);
+        }
+    }
+     */
+}
