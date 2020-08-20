766c4fc6d9d3e88987ea6ebc8c95974f8b951cad
xiaolong <xlshi1996@163.com>
2020-08-20 10:14:49 +0800

    leetcode_78_子集

--- src/question/回朔/_78_子集.java
+++ src/question/回朔/_78_子集.java
@@ -12,8 +12,25 @@ import java.util.List;
  * 参考：https://leetcode-cn.com/problems/subsets/solution/hui-su-suan-fa-by-powcai-5/
  */
 public class _78_子集 {
-
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
+        backtrack(0, nums, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(int start, int[] nums, ArrayList<Integer> track) {
+        res.add(new ArrayList<>(track));
+        for (int i = start; i < nums.length; i++) {
+            // 选择
+            track.add(nums[i]);
+            // 回溯
+            backtrack(i+1, nums, track);
+            // 撤销选择
+            track.remove(track.size() - 1);
+        }
+    }
+
+    /*public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         backtrack(0, nums, res, new ArrayList<Integer>());
         return res;
@@ -27,5 +44,5 @@ public class _78_子集 {
             backtrack(j + 1, nums, res, tmp);
             tmp.remove(tmp.size() - 1);
         }
-    }
+    }*/
 }
