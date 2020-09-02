cee98f542ada38c2710d8a91159a229598b533d0
xiaolong <xlshi1996@163.com>
2020-09-02 16:55:22 +0800

    leetcode_1290_二进制链表转整数

---
+++ src/question/链表/_1290_二进制链表转整数.java
@@ -0,0 +1,26 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 16:49
+ * 地址：https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+ */
+public class _1290_二进制链表转整数 {
+/*    public int getDecimalValue(ListNode head) {
+        ListNode temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        ListNode temp2 = head;
+        int res = 0;
+        while (temp2 != null) {
+            res += (temp2.val) * Math.pow(2,count-1);
+            count--;
+            temp2 = temp2.next;
+        }
+        return res;
+    }*/
+}
