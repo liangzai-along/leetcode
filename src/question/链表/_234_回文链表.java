ae6087f8f7eca2f3e7bb1c04b9ded38550c67d59
xiaolong <xlshi1996@163.com>
2020-09-02 15:57:00 +0800

    leetcode_234_回文链表

---
+++ src/question/链表/_234_回文链表.java
@@ -0,0 +1,34 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 15:50
+ * 地址：https://leetcode-cn.com/problems/palindrome-linked-list/
+ */
+public class _234_回文链表 {
+
+/*    public boolean isPalindrome(ListNode head) {
+        ListNode temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        int[] res = new int[count];
+        int i = 0;
+        ListNode temp2 = head;
+        while (temp2 != null) {
+            res[i++] = temp2.val;
+            temp2 = temp2.next;
+        }
+        int left = 0;
+        int right = res.length-1;
+        while (left < right) {
+            if (res[left] != res[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }*/
+}
