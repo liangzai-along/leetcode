10fc075718a35fca8ac93ad4de788deee094e0d8
xiaolong <xlshi1996@163.com>
2020-09-02 15:35:49 +0800

    leetcode_203_移除链表元素

---
+++ src/question/链表/_203_移除链表元素.java
@@ -0,0 +1,24 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 15:34
+ * 地址：https://leetcode-cn.com/problems/remove-linked-list-elements/
+ */
+public class _203_移除链表元素 {
+/*    public ListNode removeElements(ListNode head, int val) {
+        //新加一个头节点
+        ListNode header = new ListNode(-1);
+        header.next = head;
+        ListNode cur = header;
+        while (cur.next != null){
+            if (cur.next.val == val){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return header.next;
+    }*/
+}
