06792503fbeef10895f92a55a26f22f5f5851d33
xiaolong <xlshi1996@163.com>
2020-09-02 16:45:31 +0800

    leetcode_52_两个链表的第一个公共节点

---
+++ src/question/链表/_52_两个链表的第一个公共节点.java
@@ -0,0 +1,20 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 10:57
+ * 地址：https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+ */
+public class _52_两个链表的第一个公共节点 {
+/*    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }*/
+}
