88f0e04e76c188a03dd44b71ba9cc1b6a9e67a54
xiaolong <xlshi1996@163.com>
2020-09-02 10:12:01 +0800

    leetcode_22_链表中倒数第k个节点

---
+++ src/question/链表/_22_链表中倒数第K个节点.java
@@ -0,0 +1,30 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 10:04
+ * 地址：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+public class _22_链表中倒数第K个节点 {
+//    快慢指针
+/*    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+
+        ListNode slow = temp;
+        ListNode fast = temp;
+
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }*/
+
+}
