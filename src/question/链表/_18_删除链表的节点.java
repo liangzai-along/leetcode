f100e11649d51b95580a1a0c69360ea35c5647c0
xiaolong <xlshi1996@163.com>
2020-09-02 10:53:32 +0800

    leetcode_18_删除链表的节点

---
+++ src/question/链表/_18_删除链表的节点.java
@@ -0,0 +1,24 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 10:40
+ * 地址：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+ */
+public class _18_删除链表的节点 {
+    // 注意头节点
+    //public ListNode deleteNode(ListNode head, int val) {
+    //    ListNode fakeHead = new ListNode(0);
+    //    fakeHead.next = head;
+    //    ListNode cur = fakeHead;
+    //    while (cur.next != null) {
+    //        if (cur.next.val == val) {
+    //            cur.next = cur.next.next;
+    //            break;
+    //        }
+    //        cur=cur.next;
+    //    }
+    //    return fakeHead.next;
+    //}
+}
