0bc5ba9ea407229f5bacf49b0ff3dd4b745e642c
xiaolong <xlshi1996@163.com>
2020-09-02 16:24:50 +0800

    leetcode_0207_链表相交

---
+++ src/question/链表/_0207_链表相交.java
@@ -0,0 +1,20 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 16:22
+ * 地址：https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/
+ */
+public class _0207_链表相交 {
+/*    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }*/
+}
