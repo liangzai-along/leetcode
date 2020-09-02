78157059fb9d43069eaffbb66c4b8577295d42a0
xiaolong <xlshi1996@163.com>
2020-09-02 10:03:07 +0800

    leetcode_0203_删除中间节点

---
+++ src/question/链表/_0203_删除中间节点.java
@@ -0,0 +1,14 @@
+package question.链表;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/2 9:55
+ * 地址：https://leetcode-cn.com/problems/delete-middle-node-lcci/
+ */
+public class _0203_删除中间节点 {
+/*    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }*/
+}
