3eb831e275a39737aeb16173ea3a4047bdc8ff05
xiaolong <xlshi1996@163.com>
2020-09-04 15:00:23 +0800

    leetcode_68_二叉搜索树的最近公共节点

---
+++ src/question/tree/_68_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,20 @@
+package question.tree;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/4 14:57
+ * 地址：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+ */
+public class _68_二叉搜索树的最近公共祖先 {
+/*    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ((root.val - p.val) * (root.val - q.val) <= 0) return root;
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+    }*/
+}
