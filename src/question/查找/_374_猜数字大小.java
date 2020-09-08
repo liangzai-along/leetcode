7febc4c47a57864953b67fefe52e32cf99cedae7
xiaolong <xlshi1996@163.com>
2020-09-08 14:43:43 +0800

    leetcode_374_猜数字大小

---
+++ src/question/查找/_374_猜数字大小.java
@@ -0,0 +1,28 @@
+package question.查找;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/8 14:37
+ * 地址：https://leetcode-cn.com/problems/guess-number-higher-or-lower/
+ */
+public class _374_猜数字大小 {
+
+/*    // 二分法，判断的这个步骤，guess帮我们做了
+    public int guessNumber(int n) {
+        int left = 0;
+        int right = n;
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+            int g = guess(mid);
+            if (g == 0) {
+                return mid;
+            } else if (g == -1) {
+                right = mid -1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }*/
+}
