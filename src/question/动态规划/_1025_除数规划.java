d574098b093c03bdb573b4d1e8c87f60606f5988
xiaolong <xlshi1996@163.com>
2020-09-09 10:12:00 +0800

    leetcode_1025_除数规划

---
+++ src/question/动态规划/_1025_除数规划.java
@@ -0,0 +1,13 @@
+package question.动态规划;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/9 10:03
+ * 地址：https://leetcode-cn.com/problems/divisor-game/
+ */
+public class _1025_除数规划 {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
