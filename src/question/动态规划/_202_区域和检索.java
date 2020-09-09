e0b27e5e2aaa002ec7a1896d2482e83181cdedaa
xiaolong <xlshi1996@163.com>
2020-09-09 09:21:12 +0800

    leetcode_202_区域和检索

---
+++ src/question/动态规划/_202_区域和检索.java
@@ -0,0 +1,27 @@
+package question.动态规划;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/9/9 9:17
+ * 地址：https://leetcode-cn.com/problems/range-sum-query-immutable/submissions/
+ */
+public class _202_区域和检索 {
+/*    int[] sums;
+    public NumArray(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] += sums[i-1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0) {
+            return sums[j];
+        } else {
+            return sums[j] - sums[i-1];
+        }
+    }*/
+}
