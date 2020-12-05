1413ca5f00e7ab3adcb4345cbc06b7426cabb948
xiaolong <xlshi1996@163.com>
2020-12-05 15:31:00 +0800

    leetcode_148_排序链表

---
+++ src/question/排序/_148_排序链表.java
@@ -0,0 +1,63 @@
+package question.排序;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/12/5 15:29
+ */
+public class _148_排序链表 {
+
+    // 归并排序
+    public ListNode sortList(ListNode head) {
+        return head == null ? null : mergeSort(head);
+    }
+
+    private ListNode mergeSort(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+        // 利用快慢指针将链表分成两段
+        ListNode p = head, q = head, pre = null;
+        while (q != null && q.next != null) {
+            pre = p;
+            p = p.next;
+            q = q.next.next;
+        }
+
+        pre.next = null;
+
+        ListNode l = mergeSort(head);
+        ListNode r = mergeSort(p);
+        return merge(l, r);
+    }
+
+    ListNode merge(ListNode l, ListNode r) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l != null && r != null) {
+            if (l.val <= r.val) {
+                cur.next = l;
+                cur = cur.next;
+                l = l.next;
+            } else {
+                cur.next = r;
+                cur = cur.next;
+                r = r.next;
+            }
+        }
+        if (l != null) {
+            cur.next = l;
+        }
+        if (r != null) {
+            cur.next = r;
+        }
+        return dummyHead.next;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
