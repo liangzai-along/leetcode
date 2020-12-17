87666dbf657b405fef69f931be2f731631b36b90
xiaolong <xlshi1996@163.com>
2020-12-17 15:16:56 +0800

    leetcode_239_滑动窗口最大值

---
+++ src/question/滑动窗口/_239_滑动窗口最大值.java
@@ -0,0 +1,35 @@
+package question.滑动窗口;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Author: xiaolong
+ * @email: xlshi1996@163.com
+ * @Date: 2020/12/17 15:16
+ */
+public class _239_滑动窗口最大值 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) return new int[0];
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length-k+1];
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast((nums[i]));
+        }
+        res[0] = deque.peekFirst();
+        for(int i = k; i < nums.length; i++) { // 形成窗口后
+            if(deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+}
